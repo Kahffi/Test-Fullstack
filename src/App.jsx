1df4c52c33dcf38d29e757c319cef47c45de8daa
kahffi <daffaalkahffi24@gmail.com>
2024-07-11 00:06:13 +0700

    adding react-leaflet

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<div>
-				<h1>KOSSSOONGG???!??</h1>
-			</div>
-		</>
+		<div className="App">
+			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+				<TileLayer
+					attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+					url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+			</MapContainer>
+		</div>
 	);
 }
 
