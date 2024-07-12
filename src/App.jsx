aa9cfd5bb181d9b0398c23e83f02e5be72e0f231
kahffi <daffaalkahffi24@gmail.com>
2024-07-12 07:21:33 +0700

    adding map locate and little bit of firebase authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import {
+	MapContainer,
+	Marker,
+	TileLayer,
+	useMapEvents,
+	useMap,
+} from "react-leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
+
+function CaseMarker() {
+	const map = useMap();
+	map.locate().on("locationfound", (e) => {
+		map.flyTo(e.latlng, 16);
+	});
+	//useMapEvents({
+	// 	tileloadstart: () => {
+	// 		console.log("load");
+	// 		map.locate();
+	// 	},
+	// 	locationfound: (location) => {
+	// 		map.flyTo(location.latlng);
+	// 	},
+	// });
+	return null;
+}
 
 function App() {
 	return (
@@ -13,6 +36,7 @@ function App() {
 					attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 					url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
+				<CaseMarker />
 			</MapContainer>
 		</div>
 	);
